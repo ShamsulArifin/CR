0d2a29889953d872251784001b613902ef264e49
Shamsul Arifin <arifinbd6@gmail.com>
2023-12-13 00:44:10 +0600

    Blueprint Callable

--- Source/CR/Grabber.cpp
+++ Source/CR/Grabber.cpp
@@ -32,6 +32,11 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	
+}
+
+void UGrabber::Grab()
+{
 	FVector Start = GetComponentLocation();
 	FVector End = Start + GetForwardVector() * MaxGrabDistance;
 	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
@@ -56,6 +61,12 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	{
 		UE_LOG(LogTemp, Display, TEXT("No Actor Hit!!"))
 	}
+}
+
+
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Display, TEXT("Released Grabber"));
 }
 
 
