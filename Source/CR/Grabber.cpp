f9c3f552a33467f15fb447ba6078425f85f50470
Shamsul Arifin <arifinbd6@gmail.com>
2023-12-15 16:02:28 +0600

    Physics Handle

--- Source/CR/Grabber.cpp
+++ Source/CR/Grabber.cpp
@@ -5,6 +5,8 @@
 
 #include "Engine/World.h"
 #include "DrawDebugHelpers.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -22,8 +24,15 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	UPhysicsHandleComponent* PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandle != nullptr)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Physics Handle!!: %s"), *PhysicsHandle->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Physics Handle Found!!"));
+	}
 }
 
 
@@ -31,7 +40,7 @@ void UGrabber::BeginPlay()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
+	
 	
 }
 
